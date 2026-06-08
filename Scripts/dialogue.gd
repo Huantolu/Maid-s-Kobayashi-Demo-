e56efe1b35a0b110d6fb109682f664effe6a3ea2
M4tisPto <matis.pto.104250@gmail.com>
2026-06-08 13:00:30 -0400

    eaeasd

--- Scripts/dialogue.gd
+++ Scripts/dialogue.gd
@@ -8,17 +8,21 @@ extends Control
 
 var _playing_voice := false
 
+@onready var _calc: PauseCalculator
+  
+
+
 # We are going to use this logic to test, will be removed later
 func _ready() -> void:
 	await(get_tree().create_timer(0.1).timeout)
 	update_message("[color=#ff0000]Hi[/color] [wave]I was generated[/wave] for the [rainbow]dialogue[/rainbow] system test for the godot game [shake rate=20 level=10]engine[/shake]")
 
+ 
   
-
-
-# Update the message and starts typing
 func update_message(message: String) -> void:
-	content.bbcode_text = message
+	content.bbcode_text = _calc.extract_pauses_from_string(
+		message
+	)
 	content.visible_characters = 0
 	type_timer.start()
 	_playing_voice = true
